4e099c14526c1fe0d42c3625042546bc957d3abb
Ryan Osmundson <rosmundson@outlook.com>
2022-01-14 09:43:42 -0700

    commit tree fcfd291d593fecd21691d806d2524a4764e9c6a3

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+function submitData(usersName, usersEmail){
+  const formData = {
+    name: usersName,
+    email: usersEmail
+  }
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(response => response.json())
+  .then(data => document.body.innerHTML = data.id)
+  .catch(error => document.body.innerHTML = error.message)
+}
+
+// it( "handles a failed POST request using catch, appends the error message to the DOM", async function () {
+//   let message = 'Unauthorized Access'
+//   nock( 'http://localhost:3000' )
+//     .post( '/users' )
+//     .replyWithError( {
+//       message: message,
+//       code: '401',
+//     } )
+
+//   let name = "Jim"
+//   let email = "jim@jim.com"
+
+//   await submitData( name, email )
+//   expect( document.body.innerHTML )
+//     .to.include( message )
+// } )
+// } )
